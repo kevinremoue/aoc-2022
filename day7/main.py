28425757fcdcd2c358350f9bd63045f1134ff096
Kevin Remoué <kevinremoue@kevins-mbp.home>
2022-12-07 22:41:16 +0100

    Day7

--- day7/main.py
+++ day7/main.py
@@ -1,44 +1,80 @@
-class TreeNode:
 
-    def __init__(self, name, size, parent=None):
+class Node:
+    def __init__(self, name, parent, size, isFolder):
         self.name = name
-        self.size = size
         self.parent = parent
+        self.size = size
+        self.isFolder = isFolder
+        self.children = []
 
     def __str__(self):
-        string = self.name + " size: " + str(self.size) + "\n\t" + str(self.parent)
+        string = self.name
+        for child in self.children:
+            string += "\n - "
+            string += str(child)
         return string
-        
-# input = open('day7/input.txt', 'r')
-input = open('day7/test.txt', 'r')
+
+def process_each_size(folder):
+    if folder.isFolder:
+        for child in folder.children:
+            folder.size+=process_each_size(child)
+    return folder.size
+
+def get_folders(root):
+    folders = []
+    if root.isFolder:
+        folders.append(root)
+        for child in root.children:
+            folders.extend(get_folders(child))
+    return folders
+
+input = open('day7/input.txt', 'r')
+# input = open('day7/test.txt', 'r')
 commands = input.read().split("\n")
 
 
-current_folder = TreeNode("/", 0) 
-head = current_folder
-
-command_mode = ""
-depth = 0
+root = Node("/", None, 0, True) 
+current_folder = root
 
 for command in commands[1:]:
     parsed_cmd = command.split(" ")
     #if it is a command input, get the command
     if parsed_cmd[0] == "$":
-        if parsed_cmd[1] == "ls":
-            command_mode = "ls"
-        elif parsed_cmd[1] == "cd":
-            command_mode = "cd"
+        if parsed_cmd[1] == "cd":
+            #cd ..
             if parsed_cmd[2] == "..":
-                depth -=1
                 current_folder = current_folder.parent
+            #cd x
             else:
-                depth +=1
-                new_dir = TreeNode(parsed_cmd[2], 0, current_folder)
+                new_dir = Node(parsed_cmd[2], current_folder, 0, True)
+                current_folder.children.append(new_dir)
                 current_folder = new_dir
-    elif command_mode == "ls":
-        if command[0] == "dir":
-            new_dir = TreeNode(parsed_cmd[1], 0, current_folder)
-        else:
-            new_file = TreeNode(parsed_cmd[1], parsed_cmd[0], current_folder)
+    elif parsed_cmd[0] != "dir":
+        file = Node(parsed_cmd[1], None, int(parsed_cmd[0]),False)
+        current_folder.children.append(file)
 
-print(head)
+#Root got the full tree now!
+#get the size and then folders with the right size
+process_each_size(root)
+folders = get_folders(root)
+size = 0
+for folder in folders:
+    if folder.size <= 100000:
+        size += folder.size
+print("part1 ", size)
+
+#PART2
+sizes = []
+fs_size = 70000000
+needed_size = 30000000
+free_space = fs_size - int(folders[0].size)
+space_to_clear = needed_size - free_space
+print("space to clear ", space_to_clear)
+
+for folder in folders:
+    if folder.size >= space_to_clear:
+        sizes.append(folder.size)
+
+print(sizes)
+print("part2 ", min(sizes))
+
