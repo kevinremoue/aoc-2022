7f66820f08949dbe86f4ec4ebc930b5fae4a28f7
kremoue <kremoue@idmog.com>
2022-12-19 14:14:19 +0100

    Day14 part1

--- day14/main.py
+++ day14/main.py
@@ -1,13 +1,12 @@
 import numpy as np
+import copy
 
 #Try with dict
 
 #Build rock paths, array of array of coordinates
 def build_rock_paths(input):
     paths = []
-    global NB_ROW 
-    global NB_COL 
-
+    global THRESHOLD
     for line in input:
         coords = line.split("->")
         path = []
@@ -15,10 +14,8 @@ def build_rock_paths(input):
             coord = elt.rstrip().split(",")
             col = int(coord[0])
             row = int(coord[1])
-            if col > NB_COL:
-                NB_COL = col+1
-            if row > NB_ROW:
-                NB_ROW = row+1
+            if row > THRESHOLD:
+                THRESHOLD = row
             path.append([col,row])
         paths.append(path)
     return paths
@@ -34,39 +31,36 @@ def add_path_in_matrix(path, matrix):
         if path[idx_part][0] - path[idx_part+1][0] < 0:
             while x <= path[idx_part+1][0]:
                 print("Adding rock in", x,y)
-                matrix[x,y] = 1
+                matrix[x,y] = "#"
                 x+=1
         #else negatively
         elif path[idx_part][0] - path[idx_part+1][0] > 0:
             while x >= path[idx_part+1][0]:
                 print("Adding rock in", x,y)
-                matrix[x,y] = 1
+                matrix[x,y] = "#"
                 x-=1
         #y position is moving
         else:
-            #if y going positively
+            #if y going poitively
             if path[idx_part][1] - path[idx_part+1][1] < 0:
                 while y <= path[idx_part+1][1]:
                     print("Adding rock in", x,y)
-                    matrix[x,y] = 1
+                    matrix[x,y] = "#"
                     y+=1
             #else negatively
             elif path[idx_part][1] - path[idx_part+1][1] > 0:
                 while y >= path[idx_part+1][1]:
                     print("Adding rock in", x,y)
-                    matrix[x,y] = 1
+                    matrix[(x,y)] = "#"
                     y-=1
     return matrix
 
 def build_matrix(input):
-    global NB_ROW 
-    global NB_COL 
     #Build rock path
     paths = build_rock_paths(input)
-    print("MAX X",NB_COL,"MAX Y",NB_ROW)
-    #Build matrix
-    matrix = np.zeros((NB_COL,NB_ROW), dtype=int)
 
+    #Build matrix
+    matrix = dict()
     #Build rock path inside matrix
     for path in paths:
         matrix = add_path_in_matrix(path, matrix)
@@ -75,64 +69,71 @@ def build_matrix(input):
 
 def pour_sand(matrix):
     global START_SAND
-    sandx = START_SAND[0]
-    sandy = START_SAND[1]
+    global THRESHOLD
+    new_sand = copy.deepcopy(START_SAND)
 
-    while sandx <= NB_COL and sandy <= NB_ROW:
-        
-
+    while can_move(matrix,new_sand):
+        print("Yes can")
+        new_sand = move(matrix,new_sand)
+        print("Moved in", new_sand)
+        #if the sand is going below the max depth
+        #return False and stop everything
+        if new_sand[1] > THRESHOLD:
+            return matrix, False
+    #Cannot move anymore, we add it
+    print("Adding", (new_sand[0],new_sand[1]))
+    matrix[(new_sand[0],new_sand[1])] = "o"
     #is going out of bound so we stop
-    return matrix, False
+    return matrix, True
 
-def get_possible_pos(matrix,pos):
-    global visited
-    positions = []
+def can_move(matrix,sand):
+    print("Can", sand, "moves?")
+    sandx = sand[0]
+    sandy = sand[1]
+    if (sandx,sandy+1) in matrix:
+        if (sandx-1,sandy+1) in matrix:
+            if (sandx+1,sandy+1) in matrix:
+                return False
+            else:
+                return True
+        else:
+            return True
+    else:
+        return True
 
-    #try a neighboor that hasnt been visited
-    #check right side
-    if pos[1]+1 < NB_COL:
-        if (matrix[pos[0],pos[1]+1] - matrix[pos[0],pos[1]] <= 1) or (matrix[pos[0],pos[1]] - matrix[pos[0],pos[1]+1] > 0):
-            new_position = [pos[0],pos[1]+1]
-            if new_position not in visited:
-                positions.append(new_position)
-    #check left
-    if pos[1]-1 >= 0:
-        if matrix[pos[0],pos[1]-1] - matrix[pos[0],pos[1]] <= 1 or (matrix[pos[0],pos[1]] - matrix[pos[0],pos[1]-1] > 0):
-            new_position = [pos[0],pos[1]-1]
-            if new_position not in visited:
-                positions.append(new_position)
-    #check up
-    if pos[0]-1 >= 0:
-        if matrix[pos[0]-1,pos[1]] - matrix[pos[0],pos[1]] <= 1 or (matrix[pos[0],pos[1]] - matrix[pos[0]-1,pos[1]] > 0):
-            new_position = [pos[0]-1,pos[1]]
-            if new_position not in visited:
-                positions.append(new_position)
-    #check down
-    if pos[0]+1 < NB_ROW:
-        if matrix[pos[0]+1,pos[1]] - matrix[pos[0],pos[1]] <= 1 or (matrix[pos[0],pos[1]] - matrix[pos[0]+1,pos[1]] > 0):
-            new_position = [pos[0]+1,pos[1]]
-            if new_position not in visited:
-                positions.append(new_position)
+def move(matrix, sand):
+    sandx = sand[0]
+    sandy = sand[1]
+    if (sandx,sandy+1) not in matrix:
+        return (sandx, sandy+1)
+    elif (sandx-1,sandy+1) not in matrix:
+        return (sandx-1,sandy+1)
+    elif (sandx+1,sandy+1) not in matrix:
+        return (sandx+1,sandy+1)
+    else:
+        return (sandx,sandy+1)
 
-    return positions
+input = open('day14/input.txt', 'r').read().rstrip().split("\n")
+# input = open('day14/test.txt', 'r').read().rstrip().split("\n")
 
-# input = open('day14/input.txt', 'r').read().rstrip().split("\n")
-input = open('day14/test.txt', 'r').read().rstrip().split("\n")
-
-NB_ROW = 0
-NB_COL = 0
-START_SAND = [500,0]
+THRESHOLD = 0
+START_SAND = (500,0)
 count = 0
 #Create matrix detecting max values and adding rock paths
 matrix = build_matrix(input)
 
 isTrue = True
 
-print(matrix)
+# print(matrix)
+# print("THRESHOLD:", THRESHOLD)
 
-#Pour the sand until it is finished
+# Pour the sand until it is finished
+# When should we take a new sand ? 
+# 
 while isTrue :
+    print("NEW SAND")
     matrix,isTrue = pour_sand(matrix)
     count+=1
 
-print(count)
+#ignore last one as it is used to detect the end
+print(count-1)
